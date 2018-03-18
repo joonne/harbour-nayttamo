8160a18bb062511a256e8f72aa46b421b98729b3
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2018-03-18 09:39:19 +0200

    translations

---
+++ translations/harbour-nayttamo-fi.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
+<context>
+    <name>CategoriesPage</name>
+    <message>
+        <source>Categories</source>
+        <translation>Kategoriat</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Search</source>
+        <translation>Haku</translation>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation>Kategoriat</translation>
+    </message>
+    <message>
+        <source>Current Broadcasts</source>
+        <translation>Nykyiset lähetykset</translation>
+    </message>
+    <message>
+        <source>Show program info</source>
+        <translation>Näytä ohjelman tiedot</translation>
+    </message>
+</context>
+<context>
+    <name>ProgramOverviewPage</name>
+    <message>
+        <source>Description</source>
+        <translation>Kuvaus</translation>
+    </message>
+</context>
+<context>
+    <name>ProgramsPage</name>
+    <message>
+        <source>Programs</source>
+        <translation>Ohjelmat</translation>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Season %1 Episode %2</source>
+        <translation>Kausi %1 Jakso %2</translation>
+    </message>
+    <message>
+        <source>Season %1</source>
+        <translation>Kausi %1</translation>
+    </message>
+    <message>
+        <source>Episode %1</source>
+        <translation>Jakso %1</translation>
+    </message>
+    <message>
+        <source>Season - Episode -</source>
+        <translation>Kausi - Jakso -</translation>
+    </message>
+    <message>
+        <source>Search for a program</source>
+        <translation>Etsi ohjelmaa</translation>
+    </message>
+    <message>
+        <source>Season</source>
+        <translation>Kausi</translation>
+    </message>
+    <message>
+        <source>Jakso</source>
+        <translation>Jakso</translation>
+    </message>
+    <message>
+        <source>Here will be stuff when you search for something</source>
+        <translation>Tänne ilmestyy tavaraa kun etsit jotain</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/harbour-nayttamo-sv.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="sv" version="2.1">
+<context>
+    <name>CategoriesPage</name>
+    <message>
+        <source>Categories</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Categories</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Current Broadcasts</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Show program info</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>ProgramOverviewPage</name>
+    <message>
+        <source>Description</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>ProgramsPage</name>
+    <message>
+        <source>Programs</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>SearchPage</name>
+    <message>
+        <source>Season %1 Episode %2</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Season %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Episode %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Season - Episode -</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Search for a program</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Season</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Jakso</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <source>Here will be stuff when you search for something</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+</TS>
